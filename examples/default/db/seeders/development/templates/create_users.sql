1e0312ab801a0234b535a6163873aece0b537994
Jiro <j.onodera26@gmail.com>
2022-01-28 08:57:51 -0800

    Update example

---
+++ examples/default/db/seeders/development/templates/create_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (display_name, email, created_at, updated_at)
+VALUES ('alice', 'alice@example.com', NOW(), NOW()),
+('ben', 'ben@example.com', NOW(), NOW()),
+('conny', 'conny@example.com', NOW(), NOW());
+
